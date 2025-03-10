15a6f052461bcd19cee3e7e424a5bbb95fa97a29
mohammed <zchizzz11111@gmail.com>
2025-03-10 20:18:53 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Features from './Sections/Features'
+import Header from './Sections/Header'
+import Hero from './Sections/Hero'
+
+function App() {
+
+  return (
+    <main className='overflow-hidden'>
+      <Header />
+      <Hero />
+      <Features />
+    </main>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+export const features = [
+    {
+        id: '0',
+        icon: "/images/feature-1.png",
+        capion: '',
+        title: 'FEATURE 1',
+        text: '',
+        button: {
+            icon: '/images/magictouch.svg',
+            title: 'Watch the Demo'
+        },
+    },
+    {
+        id: '1',
+        icon: "/images/feature-2.png",
+        capion: '',
+        title: 'FEATURE 2',
+        text: '',
+        button: {
+            icon: '/images/magictouch.svg',
+            title: 'Watch the Demo'
+        },
+    },
+]
